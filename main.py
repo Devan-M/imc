8ca9aa07dac72092549471b40d940cc5b56f4a66
Devan <devan.marcal@gmail.com>
2025-07-06 19:23:00 -0300

    alterações nas mensagens

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ def classificar_imc(imc):
     elif imc < 25:
         return "Peso normal.  Tudo indica que está tudo bem, mas é importante avaliar outros parâmetros da composição corporal, para compreender se estão dentro do recomendado. Algumas pessoas apresentam IMC dentro da normalidade, mas têm circunferência abdominal maior que a recomendada e/ou quantidade de massa gorda acima do ideal."
     elif imc < 30:
-        return "Sobrepeso. É importante considerar mudanças no estilo de vida, como dieta e atividade física, para evitar complicações à saúde."
+        return "Sobrepeso.  O sobrepeso está associado ao risco de doenças como diabetes e hipertensão. Então, atenção! Consulte um médico e reveja hábitos para reverter o quadro. Também é importante avaliar outros parâmetros, como a circunferência abdominal."
     elif imc < 35:
-        return "Obesidade grau I. É recomendada a busca por orientação profissional para avaliação e possíveis intervenções."
+        return "Obesidade grau I. É importante buscar orientação médica e nutricional para entender melhor o seu caso, mesmo que os exames (colesterol e glicemia, por exemplo) estejam normais."
     elif imc < 40:
-        return "Obesidade grau II. É recomendada a busca por orientação profissional para avaliação e possíveis intervenções."
+        return "Obesidade grau II.Indica um quadro de obesidade mais evoluído em relação à classificação anterior e, mesmo com exames laboratoriais dentro da normalidade, não se deve atrasar a busca por orientação médica e nutricional."
     else:
-        return "Obesidade grau III. É recomendada a busca por orientação profissional para avaliação e possíveis intervenções."
+        return "Obesidade grau III. Nesse ponto, a chance de já estarmos diante de outras doenças associadas é mais elevada. É fundamental buscar orientação médica."
 
 # Programa para calcular o IMC e classificar o estado nutricional
 try:
